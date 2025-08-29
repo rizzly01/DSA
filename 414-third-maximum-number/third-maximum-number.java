0b8c70dda974031738caedc3dc08a29619a6813b
rizzly01 <varunkarrutuja@gmail.com>
2025-08-29 23:45:37 +0530

    Time: 3 ms (77.77%) | Memory: 43.4 MB (46.68%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.java
+++ 414-third-maximum-number/third-maximum-number.java
@@ -4,14 +4,13 @@ class Solution {
         Integer first = null, second = null, third = null;
 
         for (int num : nums) {
-            // Skip duplicates
+
             if ((first != null && num == first) || 
                 (second != null && num == second) || 
                 (third != null && num == third)) {
                 continue;
             }
 
-            // Update top 3 maximums
             if (first == null || num > first) {
                 third = second;
                 second = first;
@@ -23,8 +22,6 @@ class Solution {
                 third = num;
             }
         }
-
-        // If third max doesn't exist, return the maximum
         return (third == null) ? first : third;
     }
 }
